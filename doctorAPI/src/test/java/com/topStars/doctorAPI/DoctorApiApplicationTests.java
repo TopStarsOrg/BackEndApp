18a3f775edcd606f5122a176f078af2d9d16dc67
srikar1911 <135867952+srikar1911@users.noreply.github.com>
2024-06-08 19:13:58 -0400

    Add files via upload

---
+++ doctorAPI/src/test/java/com/topStars/doctorAPI/DoctorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topStars.doctorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
